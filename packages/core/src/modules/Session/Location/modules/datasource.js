62b5f379431b5421b7757a8f2c4094ab17a63b57
Anderson Barutti <me@andersonbarutti.com>
2022-03-09 09:03:35 -0300

    Revert "disable api"
    This reverts commit 02407946ce3eee2b5f88ca89236c3db1de01aed4.

--- packages/core/src/modules/Session/Location/modules/datasource.js
+++ packages/core/src/modules/Session/Location/modules/datasource.js
@@ -4,14 +4,14 @@ import { loader } from 'graphql.macro'
 const locations = loader('./queries/locations.graphql')
 
 export const doLoad = async () => {
-  // const { data: { allLocations } = {} } = await graphql.query({
-  //   gql: locations,
-  // })
+  const { data: { allLocations } = {} } = await graphql.query({
+    gql: locations,
+  })
 
-  // const list = allLocations.map(location => ({
-  //   ...location,
-  //   slug: location.name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/\s/g, '-')
-  // }))
+  const list = allLocations.map(location => ({
+    ...location,
+    slug: location.name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/\s/g, '-')
+  }))
 
-  return { list: [] }
+  return { list }
 }
