e5a00182d92c76efff680969267edc4d251d5a66
Anderson Barutti <me@andersonbarutti.com>
2022-04-10 21:33:16 -0300

    wip

--- packages/apps/portal/src/layouts/MainLayout/components/Header.js
+++ packages/apps/portal/src/layouts/MainLayout/components/Header.js
@@ -1,28 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import * as S from './Header.styles'
 import Logo from '../assets/biologo.png'
 import MenuIcon from '../assets/menu.png'
+import CloseIcon from '../assets/close.png'
+
+const MENU_ITEMS = [
+  { name: 'Horários de treinos', path: '/' },
+  { name: 'Unidades', path: '/' },
+  { name: 'Treinos', path: '/' },
+  { name: 'Programas', path: '/' },
+]
+
+export const MobileMenu = props => {
+  const { onClose } = props
+
+  return (
+    <S.MobileMenu>
+      <S.MobileMenuContent>
+        <S.MobileMenuClose onClick={onClose}>
+          <img src={CloseIcon} alt='Close' />
+        </S.MobileMenuClose>
+        <S.MobileMenuHeader>
+          <S.UserName>
+            Olá Danilo
+          </S.UserName>
+          <S.Plan>
+            <S.PlanTitle>
+              Plano platinum
+            </S.PlanTitle>
+            <S.PlanBranchName>
+              Unidade paulista
+            </S.PlanBranchName>
+          </S.Plan>
+        </S.MobileMenuHeader>
+        <S.MobileMenuItems>
+          {MENU_ITEMS.map(item => (
+            <S.MobileMenuItem key={item.name}>
+              <S.MobileMenuLink to={item.path}>
+                {item.name}
+              </S.MobileMenuLink>
+            </S.MobileMenuItem>
+          ))}
+          <S.MobileMenuItem>
+            <S.MobileMenuLink to='/'>
+              Sair
+            </S.MobileMenuLink>
+          </S.MobileMenuItem>
+        </S.MobileMenuItems>
+      </S.MobileMenuContent>
+    </S.MobileMenu>
+  )
+}
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <S.Container>
+      {isMenuOpen && <MobileMenu onClose={toggleMenu} />}
       <S.Content>
-      <Link to='/'>
-        <img src={Logo} alt='bioritmo' width='162' height='27'/>
-      </Link>
-      <S.Nav>
-        <S.NavButton>
-          <img src={MenuIcon} alt='menu' width='25' height='15'/>
-        </S.NavButton>
-        <S.NavList>
-          <S.NavItem><S.NavLink to='/'>Horários de treinos</S.NavLink></S.NavItem>
-          <S.NavItem><S.NavLink to='/'>Unidades</S.NavLink></S.NavItem>
-          <S.NavItem><S.NavLink to='/'>Treinos</S.NavLink></S.NavItem>
-          <S.NavItem><S.NavLink to='/'>Programas</S.NavLink></S.NavItem>
-          <S.NavItem><S.NavLink to='/entrar'>Entrar</S.NavLink></S.NavItem>
-        </S.NavList>
-      </S.Nav>
+        <Link to='/'>
+          <img src={Logo} alt='bioritmo' width='162' height='27'/>
+        </Link>
+        <S.Nav>
+          <S.NavButton onClick={toggleMenu}>
+            <img src={MenuIcon} alt='menu' width='25' height='15'/>
+          </S.NavButton>
+          <S.NavList>
+            {MENU_ITEMS.map(item => (
+              <S.NavItem key={item.name}>
+                <S.NavLink to={item.path}>
+                  {item.name}
+                </S.NavLink>
+              </S.NavItem>
+            ))}
+            <S.NavItem><S.NavLink to='/entrar'>Entrar</S.NavLink></S.NavItem>
+          </S.NavList>
+        </S.Nav>
       </S.Content>
     </S.Container>
   )
--- packages/apps/portal/src/layouts/MainLayout/components/Header.styles.js
+++ packages/apps/portal/src/layouts/MainLayout/components/Header.styles.js
@@ -9,17 +9,17 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
 `
-export const Content = styled.div`\
+export const Content = styled.div`
   width: 100%;
   max-width: 1440px;
-  padding: 26px 48px; 
+  padding: 26px 48px;
   display: flex;
   flex-direction: row;
   align-items: center;
   ${media.mobile} {
     padding: 23px 20px;
   }
-`;
+`
 
 export const Nav = styled.nav`
   flex: 1;
@@ -64,3 +64,76 @@ export const NavLink = styled(Link)`
     cursor: pointer;
   }
 `
+
+export const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: ${props => props.theme.colors.background};
+  color: ${props => props.theme.colors.onBackground};
+  z-index: 9999;
+`
+export const MobileMenuContent = styled.div``
+export const MobileMenuClose = styled.div`
+  position: absolute;
+  top: 28px;
+  right: 21px;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`
+export const MobileMenuHeader = styled.div`
+  padding: 26px 24px 40px;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+`
+export const UserName = styled.div`
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 19px;
+`
+export const Plan = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const PlanTitle = styled.div`
+  font-size: 16px;
+  line-height: 19px;
+  text-transform: uppercase;
+  color: rgba(255, 255, 255, 0.7);
+  margin-bottom: 5px;
+` 
+export const PlanBranchName = styled.div`
+  font-size: 16px;
+  line-height: 19px;
+  text-transform: uppercase;
+  color: rgba(255, 255, 255, 0.7);
+`
+export const MobileMenuItems = styled.div`
+  padding: 36px 24px;
+  display: flex;
+  flex-direction: column;
+`
+export const MobileMenuItem = styled.div`
+  margin-bottom: 36px;
+`
+export const MobileMenuLink = styled(Link)`
+  font-size: 16px;
+  line-height: 19px;
+  text-decoration: none;
+  cursor: pointer;
+  color: ${props => props.theme.colors.onBackground};
+
+  &:hover {
+    opacity: 0.8;
+  }
+`
+
